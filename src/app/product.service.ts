ef258c90fe41350cb065cd4565410645dbfccf01
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2020-09-28 13:13:33 +0100

    Part 3 of practice project correction

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,19 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from "@angular/http";
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
-// @Injectable({
-//   providedIn: 'root'
-// });
+@Injectable()
 export class ProductService {
-  private _album_url = "../assets/album.json";
-  constructor(private _http: Http,) {
+  private _albumUrl = "../assets/album.json";
+  constructor(private _http: Http) {
 
 
   }
 
   getAlbum(id:number) {
-    return this._http.get(this._album_url);
+    return this._http.get(this._albumUrl).map((response) => response.json());
   }
 
 }
