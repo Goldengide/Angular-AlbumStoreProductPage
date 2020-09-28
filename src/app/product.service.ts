2e4f9c8d16f92232b02dfd0e4351d8492ef8e9b5
Amowogbaje Gideon <amowogbajegideon@gmail.com>
2020-09-28 14:07:11 +0100

    Part 4 of practice project

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,7 +13,7 @@ export class ProductService {
   }
 
   getAlbum(id:number):Observable<Album> {
-    return this._http.get(this._albumUrl).map((response) => response.json():Observable<Album>);
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
 }
